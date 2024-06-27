8040df6dc463799009f3d06ff993fdfa655aa517
Dathan Ault-McCoy <dsam10@icloud.com>
2024-06-26 22:41:00 -0700

    Refactored main to be executable

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import colors as colors
 
+import argparse
+
 from continuum import *
 from superlattice import *
 from lattice import *
 from plotting import *
+
 
 #############################
 #### MODEL SPECIFICATION ####
@@ -16,7 +19,7 @@ VELOCITY = 6.582
 IL_HOPPING = 0.4
 ALPHA = 0.3
 
-def model(sl_pot, disp_pot, scale, radius):
+def make_model(sl_pot, disp_pot, scale, radius):
         continuum = GatedBernalBGModel(VELOCITY, IL_HOPPING, disp_pot)
         lattice = TriangularLattice(2 * np.pi / scale)
         sl_potential = np.diag([sl_pot, sl_pot, ALPHA * sl_pot, ALPHA * sl_pot])
@@ -30,122 +33,65 @@ def model(sl_pot, disp_pot, scale, radius):
 ###################
 
 def band_from_offset(radius, offset):
-     return model(0.005, -0.010, 500, radius).lowest_pos_band() + offset
+    return make_model(0.005, -0.010, 500, radius).lowest_pos_band() + offset
 
 
 
-################
-### ROUTINES ###
-################
 
-def sweep_pots_grid(
-          sl_min, sl_max, sl_n, disp_min, disp_max, disp_n, 
-          scale, band_offset, radius, grid_size):
-    sl = np.linspace(sl_min, sl_max, sl_n)
-    disp = np.linspace(disp_min, disp_max, disp_n)
-    slv, dispv = np.meshgrid(sl, disp, indexing='ij')
-    above = np.zeros((sl_n, disp_n))
-    below = np.zeros((sl_n, disp_n))
-    width = np.zeros((sl_n, disp_n))
-    chern = np.zeros((sl_n, disp_n))
+##############
+#### MAIN ####
+##############
 
-    # awful hack
-    band = band_from_offset(radius, band_offset)
+def bz_sc(args):
+    model = make_model(args.sl_pot, args.disp_pot, args.scale, args.radius)
+    band = band_from_offset(args.radius, args.band_offset)
+    fig = make_plot_band_geometry(model, band, args.zoom, args.bz_res, args.struct_res)
+    fig.suptitle(f"$V_{{SL}} = {args.sl_pot}$, $V_0 = {args.disp_pot}$")
+    return fig
 
-    for i in range(sl_n):
-        for j in range(disp_n):
-            d = model(sl[i], disp[j], scale, radius).solve(grid_size)
-            above[i,j] = d.gap_above(band)
-            below[i,j] = d.gap_above(band - 1)
-            width[i,j] = d.bandwidth(band)
-            chern[i,j] = d.chern_number(band)
+def scan_sc(args):
+    def modelf(sl_pot, disp_pot):
+        return make_model(sl_pot, disp_pot, args.scale, args.radius)
+    band = band_from_offset(args.radius, args.band_offset)
+    fig = make_plot_sweep_parameters_2d_grid(modelf, band, 
+                        args.sl_min, args.sl_max, args.sl_n, "$V_{SL}$",
+                        args.disp_min, args.disp_max, args.disp_n, "$V_0$",
+                        grid_size=args.bz_res)
+    return fig
 
-    above = np.maximum(0, above)
-    below = np.maximum(0, below)
-    min_gap = np.minimum(above, below)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output-file', default=None)
+    subparsers = parser.add_subparsers(required=True)
 
-    fig, axs = plt.subplots(2, 3)
-    plots_2d(slv, dispv, 
-            [above, below, min_gap, width], 
-            fig, axs.flat,
-            ["Gap above", "Gap below", "Min gap", "Band width"],
-            "$V_{SL}$", "$V_0$")
-    
-    norm = colors.BoundaryNorm(boundaries=[-2.5,-1.5,-0.5,0.5,1.5,2.5], ncolors=256, extend='both')
-    mesh = axs[1,1].pcolormesh(slv, dispv, chern, norm=norm, cmap='RdBu_r')
-    fig.colorbar(mesh, ax=axs[1,1])
-    axs[1,1].set_title("Chern number")
-    axs[1,1].set_xlabel("$V_{SL}$")
-    axs[1,1].set_ylabel("$V_0$")
-    
-    plt.show()
+    parser_bz = subparsers.add_parser("bz")
+    parser_bz.add_argument('sl_pot', type=float)
+    parser_bz.add_argument('disp_pot', type=float)
+    parser_bz.add_argument('-z', '--zoom', default=0.6)
+    parser_bz.add_argument('-r', '--radius', default=3)
+    parser_bz.add_argument('-s', '--scale', default=500.0)
+    parser_bz.add_argument('-bn', '--bz-res', default=25)
+    parser_bz.add_argument('-kn', '--struct-res', default=50)
+    parser_bz.add_argument('-b', '--band-offset', default=0)
+    parser_bz.set_defaults(func=bz_sc)
 
+    parser_scan = subparsers.add_parser("scan")
+    parser_scan.add_argument('sl_min', type=float)
+    parser_scan.add_argument('sl_max', type=float)
+    parser_scan.add_argument('sl_n', type=int)
+    parser_scan.add_argument('disp_min', type=float)
+    parser_scan.add_argument('disp_max', type=int)
+    parser_scan.add_argument('disp_n', type=int)
+    parser_scan.add_argument('-r', '--radius', default=3)
+    parser_scan.add_argument('-s', '--scale', default=500.0)
+    parser_scan.add_argument('-bn', '--bz-res', default=4)
+    parser_scan.add_argument('-b', '--band-offset', default=0)
+    parser_scan.set_defaults(func=scan_sc)
 
+    args = parser.parse_args()
+    fig = args.func(args)
 
-def bands_and_bz(
-          sl_pot, disp_pot, scale, band_offset, zoom, 
-          radius, k_n, band_n, eigvec_loc=None):
-    m = model(sl_pot, disp_pot, scale, radius)
-    band = band_from_offset(radius, band_offset)
-
-    fig, axs = plt.subplots(2, 3, figsize=(18,10))
-    plot_bandstructure(m, band_n, axs[0,0], highlight=band)
-
-    size = np.max(np.abs(m.lattice.trans @ np.array([1,1])))
-    x = np.linspace(-zoom * size, zoom * size, k_n)
-    y = np.linspace(-zoom * size, zoom * size, k_n)
-    xv, yv = np.meshgrid(x, y, indexing='ij')
-
-    qgt = np.zeros((k_n, k_n, 2, 2), dtype=complex)
-
-    berry = np.zeros((k_n, k_n))
-    qm_det = np.zeros((k_n, k_n))
-    qgt_eig = np.zeros((k_n, k_n))
-
-    min_qgt_eig = np.inf
-
-    for i in range(k_n):
-        for j in range(k_n):
-            qgt[i,j] = m.qgt(np.array([x[i], y[j]]), band)
-            eigs, vecs = np.linalg.eigh(qgt[i,j])
-            eig = eigs[0]
-            vec = vecs[:,0]
-
-            berry[i,j] = 2 * np.imag(qgt[i,j,0,1])
-            qm_det[i,j] = np.linalg.det(np.real(qgt[i,j]))
-            qgt_eig[i,j] = eig
-
-            if eig < min_qgt_eig and m.lattice.in_first_bz(np.array([x[i], y[j]])):
-                 min_qgt_eig = eig
-                 min_qgt_eigvec = vec
-
-    tr_viol = np.zeros((k_n, k_n))
-    null_viol = np.zeros((k_n, k_n))
-
-    if eigvec_loc is not None:
-        eigvec = np.linalg.eigh(m.qgt(eigvec_loc, band))[1][:,0]
+    if args.output_file is None:
+        plt.show()
     else:
-        eigvec = min_qgt_eigvec
-
-    tr_form = np.array([
-         [np.abs(eigvec[0])**2, np.real(np.conjugate(eigvec[0]) * eigvec[1])],
-         [np.real(np.conjugate(eigvec[0]) * eigvec[1]), np.abs(eigvec[1])**2]])
-
-    for i in range(k_n):
-        for j in range(k_n):
-            # tr_viol[i,j] = np.abs(np.tensordot(np.real(qgt[i,j]), tr_form) - berry[i,j])
-            tr_viol[i,j] = np.sqrt(qm_det[i,j]) - np.abs(np.tensordot(np.real(qgt[i,j]), tr_form))
-            null_viol[i,j] = np.linalg.norm(qgt[i,j] @ eigvec)
-
-    plots_2d(xv, yv, 
-            [berry, qm_det, qgt_eig, null_viol, tr_viol],
-            fig, axs.flat[1:],
-            ["Berry curvature", "QM det", "Min QGT eigval", "Eigvec violation", "2-form violation"],
-            "$k_x$", "$k_y$", blur=True)
-    
-    fig.suptitle(f"$V_{{SL}} = {sl_pot}$, $V_0 = {disp_pot}$" + \
-                 (f" (locked: {eigvec_loc[0]}, {eigvec_loc[1]})" if eigvec_loc is not None else ""))
-    fig.tight_layout()
-    plt.subplots_adjust(bottom=0.1, left=0.08, right=0.95, top=0.9)
-    
-    plt.show()
+        fig.savefig(args.output_file)
--- plotting.py
+++ plotting.py
@@ -1,5 +1,11 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import colors
+
+
+###################
+#### UTILITIES ####
+###################
 
 class Path:
     def __init__(self, vertices, closed=True):
@@ -68,3 +74,109 @@ def plots_2d(xv, yv, data, fig, axs, axlabels, xlabel, ylabel, blur=False):
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
         ax.set_title(name)
+
+
+
+
+####################
+#### PROCEDURES ####
+####################
+        
+def make_plot_band_geometry(model, band, zoom, k_n, band_n, eigvec_loc=None, blur=True):
+    fig, axs = plt.subplots(2, 3, figsize=(18,10))
+    plot_bandstructure(model, band_n, axs[0,0], highlight=band)
+
+    size = np.max(np.abs(model.lattice.trans @ np.array([1,1])))
+    x = np.linspace(-zoom * size, zoom * size, k_n)
+    y = np.linspace(-zoom * size, zoom * size, k_n)
+    xv, yv = np.meshgrid(x, y, indexing='ij')
+
+    qgt = np.zeros((k_n, k_n, 2, 2), dtype=complex)
+
+    berry = np.zeros((k_n, k_n))
+    qm_det = np.zeros((k_n, k_n))
+    qgt_eig = np.zeros((k_n, k_n))
+
+    min_qgt_eig = np.inf
+
+    for i in range(k_n):
+        for j in range(k_n):
+            qgt[i,j] = model.qgt(np.array([x[i], y[j]]), band)
+            eigs, vecs = np.linalg.eigh(qgt[i,j])
+            eig = eigs[0]
+            vec = vecs[:,0]
+
+            berry[i,j] = 2 * np.imag(qgt[i,j,0,1])
+            qm_det[i,j] = np.linalg.det(np.real(qgt[i,j]))
+            qgt_eig[i,j] = eig
+
+            if eig < min_qgt_eig and model.lattice.in_first_bz(np.array([x[i], y[j]])):
+                 min_qgt_eig = eig
+                 min_qgt_eigvec = vec
+
+    tr_viol = np.zeros((k_n, k_n))
+    null_viol = np.zeros((k_n, k_n))
+
+    if eigvec_loc is not None:
+        eigvec = np.linalg.eigh(model.qgt(eigvec_loc, band))[1][:,0]
+    else:
+        eigvec = min_qgt_eigvec
+
+    tr_form = np.array([
+         [np.abs(eigvec[0])**2, np.real(np.conjugate(eigvec[0]) * eigvec[1])],
+         [np.real(np.conjugate(eigvec[0]) * eigvec[1]), np.abs(eigvec[1])**2]])
+
+    for i in range(k_n):
+        for j in range(k_n):
+            # tr_viol[i,j] = np.abs(np.tensordot(np.real(qgt[i,j]), tr_form) - berry[i,j])
+            tr_viol[i,j] = np.sqrt(qm_det[i,j]) - np.abs(np.tensordot(np.real(qgt[i,j]), tr_form))
+            null_viol[i,j] = np.linalg.norm(qgt[i,j] @ eigvec)
+
+    plots_2d(xv, yv, 
+            [berry, qm_det, qgt_eig, null_viol, tr_viol],
+            fig, axs.flat[1:],
+            ["Berry curvature", "QM det", "Min QGT eigval", "Eigvec violation", "2-form violation"],
+            "$k_x$", "$k_y$", blur=blur)
+    
+    plt.subplots_adjust(bottom=0.1, left=0.08, right=0.95, top=0.9)
+    
+    return fig
+
+def make_plot_sweep_parameters_2d_grid(modelf, band,
+          xmin, xmax, xn, xlabel, ymin, ymax, yn, ylabel,
+          grid_size):
+    x = np.linspace(xmin, xmax, xn)
+    y = np.linspace(ymin, ymax, yn)
+    xv, yv = np.meshgrid(x, y, indexing='ij')
+    above = np.zeros((xn, yn))
+    below = np.zeros((xn, yn))
+    width = np.zeros((xn, yn))
+    chern = np.zeros((xn, yn))
+
+    for i in range(xn):
+        for j in range(yn):
+            d = modelf(x[i], y[j]).solve(grid_size)
+            above[i,j] = d.gap_above(band)
+            below[i,j] = d.gap_above(band - 1)
+            width[i,j] = d.bandwidth(band)
+            chern[i,j] = d.chern_number(band)
+
+    above = np.maximum(0, above)
+    below = np.maximum(0, below)
+    min_gap = np.minimum(above, below)
+
+    fig, axs = plt.subplots(2, 3)
+    plots_2d(xv, yv, 
+            [above, below, min_gap, width], 
+            fig, axs.flat,
+            ["Gap above", "Gap below", "Min gap", "Band width"],
+            xlabel, xlabel)
+    
+    norm = colors.BoundaryNorm(boundaries=[-2.5,-1.5,-0.5,0.5,1.5,2.5], ncolors=256, extend='both')
+    mesh = axs[1,1].pcolormesh(xv, yv, chern, norm=norm, cmap='RdBu_r')
+    fig.colorbar(mesh, ax=axs[1,1])
+    axs[1,1].set_title("Chern number")
+    axs[1,1].set_xlabel(xlabel)
+    axs[1,1].set_ylabel(ylabel)
+    
+    return fig
